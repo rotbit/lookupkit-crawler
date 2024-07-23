761060bb4f6e88fbed77bf5916aef6db4df0ca38
robit <panzebin@wezhuiyi.com>
2024-07-23 23:26:33 +0800

    add task list

--- main_api.py
+++ main_api.py
@@ -1,19 +1,39 @@
 import logging
 import os
+import time
 from typing import List, Optional
 
-import requests
+from supabase import create_client, Client
 from dotenv import load_dotenv
 from fastapi import FastAPI, Header, BackgroundTasks, HTTPException
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 from website_crawler import WebsitCrawler
+from cachetools import TTLCache
+from httpx import Timeout
+
+task_cache = TTLCache(maxsize=10240, ttl=3600)
+
+from entity import URLRequest, TaskListRequest, GenerateRequest, TaskDetailRequest
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"],
+)
+
 website_crawler = WebsitCrawler()
 load_dotenv()
 system_auth_secret = os.getenv('AUTH_SECRET')
 
+# 从supabase获取数据
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+    
 # 设置日志记录
 logging.basicConfig(
     level=logging.INFO,
@@ -22,16 +42,55 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-class URLRequest(BaseModel):
-    url: str
-    tags: Optional[List[str]] = None
-    languages: Optional[List[str]] = None
+@app.post('/task/detail')
+async def task_detail(request: TaskDetailRequest, authorization: Optional[str] = Header(None)):
+    id = request.id
+    if system_auth_secret:
+        # 配置了非空的auth_secret，才验证
+        validate_authorization(authorization)
+    supabase: Client = create_client(url, key)
+    submit = supabase.table('submit').select('*').eq('id', id).execute()
 
+    taskDetail = supabase.table('task_detail').select('*').eq('submit_id', id).execute()
+    
+    submitData = {}
+    taskDetailData = {}
+    if len(submit.data) > 0:
+        submitData = submit.data[0]
+    if len(taskDetail.data) > 0:
+        taskDetailData = taskDetail.data[0]
+    
+    response = {
+        'code': 200,
+        'msg': 'success',
+        'data': {
+            "id": submitData.get('id', ''),
+            "url": submitData.get('url', ''),
+            "name": submitData.get('name', ''),
+            "model": taskDetailData.get('model', ''),
+            "keyword": taskDetailData.get('keyword', ''),
+            "keyword_density": taskDetailData.get('keyword_density', ''),
+            "language": taskDetailData.get('language', ''),
+            "reference_content": taskDetailData.get('reference_content', ''),
+        }
+    }
+    return response
 
-class AsyncURLRequest(URLRequest):
-    callback_url: str
-    key: str
+@app.post('/task/list')
+async def list_tasks(request: TaskListRequest, authorization: Optional[str] = Header(None)):
+    if system_auth_secret:
+        # 配置了非空的auth_secret，才验证
+        validate_authorization(authorization)
 
+    supabase: Client = create_client(url, key)
+    data = supabase.table('submit').select('*').execute()
+
+    response = {
+        'code': 200,
+        'msg': 'success',
+        'data': data.data
+    }
+    return response
 
 @app.post('/site/crawl')
 async def scrape(request: URLRequest, authorization: Optional[str] = Header(None)):
@@ -60,53 +119,59 @@ async def scrape(request: URLRequest, authorization: Optional[str] = Header(None
     }
     return response
 
-
-@app.post('/site/crawl_async')
-async def scrape_async(background_tasks: BackgroundTasks, request: AsyncURLRequest,
-                       authorization: Optional[str] = Header(None)):
+@app.post('/task/get_step')
+async def get_step(request: URLRequest, authorization: Optional[str] = Header(None)):
     url = request.url
-    callback_url = request.callback_url
-    key = request.key  # 请求回调接口，放header Authorization: 'Bear key'
-    tags = request.tags  # tag数组
-    languages = request.languages  # 需要翻译的多语言列表
+    tags = request.tags
 
+@app.post('/task/generate')
+async def generate(background_tasks :BackgroundTasks,request: GenerateRequest, authorization: Optional[str] = Header(None)):
     if system_auth_secret:
         # 配置了非空的auth_secret，才验证
         validate_authorization(authorization)
+    # 将任务信息写入到supabase
+    supabase: Client = create_client(url, key)
+    supabase.table('task_detail').insert(
+        [{ 'submit_id': request.task_id, 
+            'references_url': request.reference_url,
+            'keyword': request.keyword,
+            'keyword_density': request.keyword_density,
+            'language': request.language,
+            'model': request.model,
+        }]).execute()
 
     # 直接发起异步请求:使用background_tasks后台运行
-    background_tasks.add_task(async_worker, url.strip(), tags, languages, callback_url, key)
-
+    background_tasks.add_task(generate_task, request)
+    
+    # 设置任务状态
+    task_cache[request.task_id] = {"step": "start", "start_time": time.time()}
     # 若result为None,则 code="10001"，msg="处理异常，请稍后重试"
     code = 200
     msg = 'success'
     response = {
         'code': code,
-        'msg': msg
+        'msg': msg,
+        'data': {
+            'task_id': request.task_id
+        }
     }
     return response
 
-
+def generate_task(request :GenerateRequest):
+    # 爬虫处理封装为一个异步任务
+    url = request.url
+    languages = request.language
+    #website_crawler.scrape_website(url.strip(), [], languages)
+   
+def get_task_cache_key(task_id) -> str:
+    return f"task_{task_id}"
+     
 def validate_authorization(authorization):
     if not authorization:
         raise HTTPException(status_code=400, detail="Missing Authorization header")
     if 'Bearer ' + system_auth_secret != authorization:
         raise HTTPException(status_code=401, detail="Authorization is error")
-
-
-async def async_worker(url, tags, languages, callback_url, key):
-    # 爬虫处理封装为一个异步任务
-    result = await website_crawler.scrape_website(url.strip(), tags, languages)
-    # 通过requests post 请求调用call_back_url， 携带参数result， heaer 为key
-    try:
-        logger.info(f'callback begin:{callback_url}')
-        response = requests.post(callback_url, json=result, headers={'Authorization': 'Bearer ' + key})
-        if response.status_code != 200:
-            logger.error(f'callback error:{callback_url}', response.text)
-        else:
-            logger.info(f'callback success:{callback_url}')
-    except Exception as e:
-        logger.error(f'call_back exception:{callback_url}', e)
+    
 
 
 if __name__ == '__main__':
